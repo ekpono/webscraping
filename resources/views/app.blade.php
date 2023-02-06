84bf97bafaf38f67c001cc5271f816d07c9bf241
ekpono <ekponoambrose@gmail.com>
2023-02-06 12:48:31 +0100

    First commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>How To Install Vue 3 in Laravel 9 with Vite</title>
+    <title>Web scrapper</title>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script>window.Laravel = {csrfToken: '{{ csrf_token() }}'}</script>
     <script>
